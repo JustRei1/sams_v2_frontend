43c607a30c786af4339ccc63b0ab618827c14404
JustRei1 <95005297+JustRei1@users.noreply.github.com>
2026-02-24 00:20:55 +0800

    Created working toggle button for L/D mode, Fixed spacing of search bar/// Navbar update

--- src/components/PageComponents/Navbar.jsx
+++ src/components/PageComponents/Navbar.jsx
@@ -1,39 +1,48 @@
 import { useState } from "react"
-// import { NavLink } from "react-router-dom";
+
+// Page Components
+import ToggleSlider from "./ToggleSlider"
 
 // Icons
-import { MdOutlineSettings } from "react-icons/md";
+import { MdOutlineSettings } from "react-icons/md"
 
-const Navbar = ({mainContent}) => {
+
+
+const Navbar = () => {
     
     const [open, setOpen] = useState(false);
     const toggle = () => setOpen(v => !v);
-
-    const link = ({ isActive }) =>
-        `px-3 py-2 rounded-md text-sm font-medium ${
-        isActive ? "bg-slate-800 text-blue-300" : "text-gray-200 hover:bg-slate-800"
-        }`;
+    const [theme, setTheme] = useState("light");
+    const toggleTheme = () => {
+        const newTheme = theme === "light" ? "dark" : "light"
+        setTheme(newTheme)
+        // Toggling L/D theme logic here
+        //document.documentElement.classList.toggle("dark", newTheme === "dark")
+    }
 
     return (
         <header className="sticky top-0 z-50 border-b border-white bg-white">
             <nav className="mx-auto max-w-8xl px-4 py-7">
             {/* Desktop view */}
-            <div className="hidden md:flex place-items-center justify-between">
+            <div className="hidden md:grid grid-cols-12 gap-x-4">
                 {/* User */}
-                <a href="/" className="flex-1 inline-flex text-black! font-bold text-lg">Admin</a>
+                <a href="/" className="text-black col-span-1 font-bold text-lg self-center">Admin</a>
 
                 {/* Search bar */}
-                <input type="text" className="flex-3 bg-gray-100 border border-blue-600 rounded-3xl text-black"/>
+                <input type="text" className="col-span-4 bg-gray-100 border border-blue-600 rounded-3xl text-black px-6 text-lg"/>
 
                 {/* Toggle */}
-                <button className="flex-1">L/D mode</button>
+
+                <div className="col-start-10 self-center">
+                    <ToggleSlider />
+                </div>
 
                 {/* Greeting */}
-                <span className="flex-2 text-black text-lg">Hello, User</span>
+                <span className="text-black text-lg self-center">Hello, User</span>
 
                 {/* Settings Btn */}
                 <button 
-                    className={`flex flex-1 p-0 w-12 max-w-12 h-12 rounded-lg bg-transparent! hover:text-blue-800 justify-center text-black items-center`}
+                    className={`flex flex-1 p-0 w-12 max-w-12 h-12 rounded-lg bg-transparent! hover:text-blue-800 justify-center text-black items-center place-self-center`}
                     onClick={() => setIsCollapsed(!isCollapsed)}
                 >
                     <MdOutlineSettings size="24"/>
@@ -42,7 +51,7 @@ const Navbar = ({mainContent}) => {
             
             {/* Mobile toggle */}
             <button
-                className="md:hidden inline-flex items-center justify-center w-10 h-10 rounded-md border border-slate-700 text-gray-200"
+                className="md:hidden items-center justify-center w-10 h-10 rounded-md border border-slate-700 text-gray-200"
                 aria-label="Toggle menu"
                 aria-expanded={open}
                 onClick={toggle}
---
+++ src/components/PageComponents/ToggleSlider.jsx
@@ -0,0 +1,82 @@
+import { useState } from "react"
+
+// Icons
+import { FaSun } from "react-icons/fa"
+import { FaMoon } from "react-icons/fa"
+
+const ToggleSlider = () => {
+    const [theme, setTheme] = useState("light");
+    const toggleTheme = () => {
+        const newTheme = theme === "light" ? "dark" : "light"
+        setTheme(newTheme)
+        // Toggling L/D theme logic here
+        //document.documentElement.classList.toggle("dark", newTheme === "dark")
+    }
+
+    return(
+        <label className="items-center cursor-pointer" aria-label="Toggle dark mode">
+            <input
+                id="toggle_checkbox"
+                type="checkbox"
+                className="peer sr-only"
+                checked={theme === "dark"}
+                onChange={toggleTheme}
+                aria-checked={theme === "dark"}
+                role="switch"
+            />
+
+            {/* <span
+                className="relative inline-flex h-6 w-11 cursor-pointer items-center rounded-full bg-slate-300
+                    transition-colors duration-200 ease-out
+                    peer-checked:bg-blue-600
+                    peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-600/25
+                    peer-disabled:cursor-not-allowed peer-disabled:opacity-60">
+                <span
+                    className="pointer-events-none absolute left-0.5 size-5 rounded-full bg-white shadow-sm
+                            ring-1 ring-black/5 transition-all duration-200 ease-in-out
+                            peer-checked:translate-x-5">
+                </span>
+            </span> */}
+            
+            <span
+                className="
+                relative inline-flex h-7 w-14 items-center rounded-full bg-slate-300
+                transition-colors duration-200 ease-out
+                
+                peer-checked:bg-linear-to-r
+              peer-checked:from-cyan-600
+              peer-checked:to-cyan-200
+
+                peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-cyan-500/0
+                peer-disabled:cursor-not-allowed peer-disabled:opacity-60
+
+                /* thumb as pseudo-element */
+                after:absolute after:left-0.2 after:top-0.2 after:h-8 after:w-8 after:peer-focus:ring-cyan-500/30
+                after:rounded-full after:bg-white after:shadow-sm after:ring-1 after:ring-black/5
+                after:transition-transform after:duration-200 after:ease-out
+
+                /* move thumb on check */
+                peer-checked:after:translate-x-6
+                
+                /* Move the icon wrapper child too */
+                peer-checked:[&>.icon-wrap]:translate-x-6
+
+                ">
+                    
+                <span
+                    className="
+                        icon-wrap absolute left-1.5 top-1 h-5 w-5 z-10
+                        transition-transform duration-200 ease-out
+                        pointer-events-none grid place-items-center
+                    ">
+                    {/* {theme === "light" ? <FaMoon/> : <FaSun/>} */}
+                    {theme === "light" ? <FaSun className="h-5 w-5 text-amber-500" /> : <FaMoon className="h-5 w-5 text-slate-700" />}
+                </span>
+
+            </span>
+
+        </label>
+    )
+}
+
+export default ToggleSlider 
