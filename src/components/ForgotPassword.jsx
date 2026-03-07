091efa084784dc51ed5764f62777baac2860bc66
JustRei1 <95005297+JustRei1@users.noreply.github.com>
2026-03-07 13:33:13 +0800

    Function Component/ Forgot Password

---
+++ src/components/ForgotPassword.jsx
@@ -0,0 +1,15 @@
+const ForgotPassword = () => {
+
+    return(
+        // Connect API router to call forgot password API functions
+        <a 
+            href="" 
+            className="text-black text-sm
+                        hover:underline-offset-4 hover:underline"
+        >
+            Forgot Password
+        </a>
+    )
+}
+
+export default ForgotPassword
